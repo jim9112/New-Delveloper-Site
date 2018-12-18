8f34b2a000897e93bf34b6174c435d5d6a7cece8
jim9112 <jim9112@gmail.com>
2018-12-17 20:24:51 -0500

    added some JS to the project

---
+++ project/IT-270/JS/app.js
@@ -0,0 +1,25 @@
+const picture = document.querySelector('.reviews');
+let theTurnA = 15;
+let theTurnB = 15;
+let theTurnC = 15;
+picture.addEventListener('click', (e) => {
+    if (e.target.className === 'flipA'){
+        e.target.style = `transform: rotate(${theTurnA}deg)`;
+        theTurnA += 15;
+        if (theTurnA>360){
+            theTurnA = 0;
+        }
+    } else if (e.target.className === 'flipB'){
+        e.target.style = `transform: rotate(${theTurnB}deg)`;
+        theTurnB += 15;
+        if (theTurnB>360){
+            theTurnB = 0;
+        }
+    } else {
+        e.target.style = `transform: rotate(${theTurnC}deg)`;
+        theTurnC += 15;
+        if (theTurnC>360){
+            theTurnC= 0;
+        }
+    }
+});
